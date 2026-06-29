5fb0c9339989246a439d8474354f466ed6e2df52
Uday-1508 <tuday1028@gmail.com>
2026-06-29 10:07:57 +0530

    Update Groq model to llama3-70b-8192

--- agents.py
+++ agents.py
@@ -5,10 +5,9 @@ import os
 
 load_dotenv()
 llm = LLM(
-    model="groq/llama-3.3-70b-versatile",
+    model="groq/llama3-70b-8192",
     api_key=os.getenv("GROQ_API_KEY")
 )
-
 search_tool = SerperDevTool() # tool perform tasks, here it let search google
 
 researcher = Agent(
